7250b432334cc5aec6441949524af0c44a328aaa
Alirio Oliveira <aliriofilho926@yahoo.com.br>
2020-01-03 14:14:36 -0200

    Commit

--- src/main/Chip8.java
+++ src/main/Chip8.java
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2019 Alirio Oliveira <https://github.com/Alirio926>
+ *
+ * Este software é livre: você pode redistribuí-lo e/ou
+ * modificá-lo dentro dos termos da Licença Pública Geral GNU como
+ * publicada pela Fundação do Software Livre (FSF); na versão 3 da
+ * Licença, ou (a seu critério) qualquer versão posterior.
+ *
+ * Este programa é distribuído na esperança de que possa ser útil,
+ * mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ * a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ * Licença Pública Geral GNU para maiores detalhes.
+ *
+ * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+ * com este programa, Se não, veja <http://www.gnu.org/licenses/>.
  */
+
 package main;
 
 import java.awt.Toolkit;
@@ -19,8 +31,9 @@ import static main.Tela.chip8_fontset;
 
 /**
  *
- * @author aliri
+ * @author Alirio Oliveira <https://github.com/Alirio926>
  */
+
 public class Chip8 {
     
     private static final Logger LOGGER = Logger.getLogger( Chip8.class.getName() );
